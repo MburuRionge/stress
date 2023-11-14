0a1b5ece5dc7f3dcc4ba7e6e2b55456ab76744db
Mburu Rionge <victormburu3@gmail.com>
2023-11-13 21:02:21 -0800

    new

---
+++ index.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * set_bit - sets a bit at a given index to 1
+ * @n: pointer to the number to change
+ * @index: index of the bit tot set to 1
+ * Return: 1 for success, -1 for failure
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = ((1UL << index) | *n);
+	return (1);
+}
+
+0x14-bit_manipulation/4-clear_bit.c
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a given bit to 0
+ * @n: pointer to the number to change
+ * @index: index of the bit to clear
+ * Return: 1 for success, -1 for failure
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = (~(1UL << index) & *n);
+	return (1);
+}
